e6e1dbcab43ef7d20a3c8ff4d61de971043f5b9b
zhulym <zhulym@mail.ru>
2021-09-10 00:02:50 +0300

    feat: complete st-sum-digits

--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Given a number, replace this number with
@@ -12,7 +12,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+let result = null;
+export default function getSumOfDigits(n) {
+  let num = n.toString().split('').reduce((acc, el) => +acc + +el);
+  if (num > 9) {
+    getSumOfDigits(num);
+  } else {
+    result = num;
+  }
+  return result;
 }
+
+// console.log(getSumOfDigits(832));
+// console.log(getSumOfDigits(91));
+
+// Your task is to implement function that accepts a number (n) and returns the sum of its digits until we get to a one digit number.
+
+// For example:
+// For 100, the result should be 1 (1 + 0 + 0 = 1)
+// getSumOfDigits(100) => 1 For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
+// getSumOfDigits(91) => 1
+// Write your code in src/st-sum-digits.js.
