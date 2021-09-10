7cbc349fe100d0bf9b97ddc68c6816ba3027fb62
zhulym <zhulym@mail.ru>
2021-09-10 13:56:23 +0300

    feat: task Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
@@ -15,7 +15,44 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  str = new String(str);
+  options.addition = (new String(options.addition)).valueOf() === 'undefined' ? '' : (new String(options.addition)).valueOf();
+  let mainRes = [];
+  let additionRes = [];
+
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+  if (!options.repeatTimes) options.repeatTimes = 1;
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additionRes.push(options.addition);
+  }
+  for (let i = 0; i < options.repeatTimes; i++) {
+    mainRes.push(str + additionRes.join(`${options.additionSeparator ? options.additionSeparator : '|'}`));
+  }
+  return mainRes.join(`${options.separator ? options.separator : '+'}`);
 }
+
+let option = {
+  repeatTimes: 3,
+  separator: '**',
+  addition: 'PLUS',
+  additionRepeatTimes: 3,
+  additionSeparator: '00'
+}
+
+const objWithSpecificCoercion = {
+  [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+};
+
+console.log(repeater('STRING', option)) // 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+console.log(repeater('la', { repeatTimes: 3 })) // 'la+la+la'
+console.log(repeater('single', { repeatTimes: 1 }))  // 'single'
+console.log(repeater('12345', { repeatTimes: 5 })) // '12345+12345+12345+12345+12345'
+console.log(repeater('TESTstr', { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' })) // 'TESTstrADD!'
+console.log(repeater(9.234, { repeatTimes: 4, separator: '||', addition: { a: 5 }, additionRepeatTimes: 3, additionSeparator: '&&' }))
+console.log(repeater(-222, { repeatTimes: 4, separator: '||', addition: new Map(), additionRepeatTimes: 3, additionSeparator: '&&' }))
+console.log(repeater(new Set(), { repeatTimes: 3, separator: '??? ', addition: [1, 2, 3, '4'], additionRepeatTimes: 2, additionSeparator: '!!!' }))
+console.log(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }))
+console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }))
+console.log(repeater(objWithSpecificCoercion, { repeatTimes: 2, addition: objWithSpecificCoercion })) // 'STRING_OR_DEFAULTSTRING_OR_DEFAULT+STRING_OR_DEFAULTSTRING_OR_DEFAULT'
