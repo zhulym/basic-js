4665030498a52369348029f1d2ffcca014627ec9
zhulym <zhulym@mail.ru>
2021-09-10 11:45:39 +0300

    feat: task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,7 +13,64 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+
+  let newAr = [...arr];
+  for (let i = 0; i < newAr.length; i++) {
+    switch (newAr[i]) {
+      case '--double-next':
+        if (newAr[i + 1] === undefined) {
+          newAr.splice(i, 1);
+          break;
+        }
+        newAr[i] = newAr[i + 1];
+        break;
+      case '--double-prev':
+        if (newAr[i - 1] === undefined) {
+          newAr.splice(i, 1);
+          break;
+        }
+        newAr[i] = newAr[i - 1];
+        break;
+      case '--discard-next':
+        if (newAr[i + 1] === undefined) {
+          newAr.splice(i, 1);
+          break;
+        }
+        if (typeof newAr[i + 2] === 'string') {
+          newAr.splice(i, 3)
+          break;
+        }
+        newAr.splice(i, 2)
+        break;
+      case '--discard-prev':
+        if (newAr[i - 1] === undefined) {
+          newAr.splice(i, 1);
+          break;
+        }
+        if (typeof newAr[i + 2] === 'string') {
+          newAr.splice(i - 2, 3)
+          break;
+        }
+        newAr.splice(i - 1, 2)
+        break;
+      default:
+        break;
+    }
+  }
+  return newAr;
 }
+
+// console.log(transform(['--discard-prev', 1, 2, 3]))
+// console.log(transform(['--double-prev', 1, 2, 3]))
+// console.log(transform([1, 2, 3, '--double-next']))
+// console.log(transform([1, 2, 3, '--discard-next']))
+// console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])) // [1, 2, 3, 4, 5]
+// console.log(transform([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5])) // [1, 2, 3, 1337, 1337, 1337, 4, 5]
+// console.log(transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5])) // [1, 2, 3, 4, 5]
+// console.log(transform([1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5])) // [1, 2, 3, 1337, 4, 5]
+// console.log(transform(false))
+// console.log(transform(null))
+// console.log(transform(undefined))
+// console.log(transform({ 'foo': 'bar' }))
