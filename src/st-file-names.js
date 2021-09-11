4fd6c4c1142ea48a9981b865c9565ae4fa5f155a
zhulym <zhulym@mail.ru>
2021-09-12 00:15:58 +0300

    feat: task file-names

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,7 +15,35 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  let changedElements = [];
+  if (!names.length) return [];
+  for (let i = 0; i < names.length; i++) {
+    for (let j = i + 1; j < names.length; j++) {
+      if (names[j] === names[i]) {
+        let num = +(names[i].slice(-2, -1));
+        if (isNaN(num)) {
+          num = 1;
+          names[j] = `${names[i]}(${num})`;
+          changedElements.push(names[j]);
+        } else {
+          if (changedElements.includes(names[j])) {
+            names[j] = `${names[i]}(${num})`;
+            changedElements = changedElements.filter(el => el != names[i]);
+          } else {
+            names[j] = `${names[i].slice(0, -3)}(${+(names[i].slice(-2, -1)) + 1})`;
+            changedElements.push(names[j]);
+            changedElements = changedElements.filter(el => el != names[i]);
+          }
+        }
+        i = -1;
+        break;
+      }
+    }
+  }
+  return names;
 }
+
+// console.log(renameFiles(['doc', 'doc', 'image', 'doc(1)', 'doc']))  //['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)']
+// console.log(renameFiles(['a', 'b', 'cd', 'b ', 'a(3)']))  //['a', 'b', 'cd', 'b ', 'a(3)']
+// console.log(renameFiles([]))  //[]
