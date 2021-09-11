83da546062cde34a2165e321c9454dab4c536675
zhulym <zhulym@mail.ru>
2021-09-11 17:08:31 +0300

    feat: task mac-address

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,7 +14,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+  let res = n.replace(/[A-F0-9-]/g, '')
+  return !res ? true : false;
 }
+// console.log(isMAC48Address('00-1B-63-84-45-E6')) // => true
+// console.log(isMAC48Address('Z1-1B-63-84-45-E6')) // => false
+// console.log(isMAC48Address('not a MAC-48 address')) // => false
+// console.log(isMAC48Address('FF-FF-FF-FF-FF-FF')) // => true
+// console.log(isMAC48Address('G0-00-00-00-00-00')) // => false
