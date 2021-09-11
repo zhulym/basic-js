7ec0d4bda8f9b676bcdadbbcb50e83a2c10d9a40
zhulym <zhulym@mail.ru>
2021-09-11 16:23:30 +0300

    feat: task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -19,13 +19,74 @@ import { NotImplementedError } from '../extensions/index.js';
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direction = null) {
+    this.direction = direction;   // true/false
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  getFullKey(str, key) {  // получаем ключ такой же длины с пробелами и символами как у исходной строки
+    let fullKey = [];
+    let keyLength = '';
+    let indexForKey = 0;
+    let getStrArr = str.toUpperCase().split('');
+    while (keyLength.length < str.length) {
+      keyLength += key;
+    }
+    let keyArr = keyLength.toUpperCase().split('');
+    for (let i = 0; i < getStrArr.length; i++) {
+      if (letters.includes(getStrArr[i])) {
+        fullKey.push(keyArr[indexForKey])
+        indexForKey++;
+      } else {
+        fullKey.push(getStrArr[i]);
+      }
+    }
+    return fullKey;
+  }
+
+  encrypt(str, key) {
+    if (!str || !key) throw new Error(`Incorrect arguments!`);
+    let getStrArr = str.toUpperCase().split('');  //массив исходной строки
+    let getKeyArr = this.getFullKey(str, key);
+
+    let result = getStrArr.map((el, i) => {
+      const indexStr = letters.indexOf(getStrArr[i]);   // вычисляем индекс нужного символа согласно таблицы Vigenere
+      const indexKey = letters.indexOf(getKeyArr[i]);   // c = m + k
+      const resultIndex = indexStr + indexKey;
+      const indexCode = (resultIndex >= letters.length) ? resultIndex - letters.length : resultIndex;
+      if (letters.includes(el)) {
+        return el = letters[indexCode];
+      } else {
+        return el;
+      }
+    })
+    return this.direction === false ? result.reverse().join('') : result.join('');
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error(`Incorrect arguments!`);
+    let getStrArr = str.toUpperCase().split('');  //массив исходной строки
+    let getKeyArr = this.getFullKey(str, key);
+
+    let result = getStrArr.map((el, i) => {
+      const indexStr = letters.indexOf(getStrArr[i]);   // вычисляем индекс нужного символа согласно таблицы Vigenere
+      const indexKey = letters.indexOf(getKeyArr[i]);   // m = (c + length) - k
+      const resultIndex = (indexStr + letters.length) - indexKey;
+      const indexCode = resultIndex >= letters.length ? resultIndex - letters.length : resultIndex;
+      if (letters.includes(el)) {
+        return el = letters[indexCode];
+      } else {
+        return el;
+      }
+    })
+    return this.direction === false ? result.reverse().join('') : result.join('');
   }
 }
+
+// const directMachine = new VigenereCipheringMachine();
+// console.log(directMachine.encrypt('attack at dawn!', 'alphonse'))
+// console.log(directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse'))
+
