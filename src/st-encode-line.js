a912367556b222caa4531806a7f53e1c5d1bda3a
zhulym <zhulym@mail.ru>
2021-09-12 00:58:50 +0300

    feat: task encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Given a string, return its encoding version.
@@ -10,7 +10,35 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let result = '';
+  let indexes = [];
+  let arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 1) {
+      result += arr[i];
+      break;
+    }
+    if (arr[i] !== arr[i + 1]) result += arr[i];
+    if (arr[i] === arr[i + 1]) {
+      indexes.push(i);
+      for (let j = i; j < arr.length; j++) {
+        if (arr[j] !== arr[j + 1] || arr[j + 1] === 'undefined') {
+          indexes.push(j);
+          break;
+        }
+      }
+      result += (indexes[1] + 1 - indexes[0]) + arr[i];
+      i = indexes[1];
+      indexes = [];
+    }
+  }
+  return result;
 }
+
+// console.log(encodeLine('aaaatttt')) //'4a4t'
+// console.log(encodeLine('aabbccc'))  // '2a2b3c'
+// console.log(encodeLine('abbcca')) //'a2b2ca'
+// console.log(encodeLine('xyz')) // 'xyz'
+// console.log(encodeLine('')) // ''
+
