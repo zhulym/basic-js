54e2c074baa7f65a1d1ee124acd1c21260dd52b6
zhulym <zhulym@mail.ru>
2021-09-11 16:42:59 +0300

    feat: task matrix-elements-sum

--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,7 +16,35 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getMatrixElementsSum(matrix) {
+  let result = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i - 1] === undefined || matrix[i - 1][j] !== 0) result += matrix[i][j];
+    }
+  }
+  return result;
 }
+
+// console.log(getMatrixElementsSum([
+//   [0, 1, 1, 2],
+//   [0, 5, 0, 0],
+//   [2, 0, 3, 3]
+// ]))  //9
+// console.log(getMatrixElementsSum([
+//   [1, 2, 3, 4],
+//   [0, 5, 0, 0],
+//   [2, 0, 3, 3],
+// ]))  //15
+// console.log(getMatrixElementsSum([
+//   [1],
+//   [5],
+//   [0],
+//   [2],
+// ]))  //6
+// console.log(getMatrixElementsSum([
+//   [1, 1, 1],
+//   [2, 2, 2],
+//   [3, 3, 3],
+// ]))  //18
+
