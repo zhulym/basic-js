197164532a050bb5936406be015239a78666baa5
zhulym <zhulym@mail.ru>
2021-09-12 13:11:54 +0300

    feat: task common-char-count, all tasks are done

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given two strings, find the number of common characters between them.
  *
@@ -11,7 +9,29 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function getCommonCharacterCount(s1, s2) {
+  let result = 0;
+  let s1Arr = s1.split('');
+  let s2Arr = s2.split('');
+
+  for (let i = 0; i < s1Arr.length; i++) {
+    if (s2Arr.includes(s1Arr[i])) {
+      result++;
+      let idx1 = s1Arr.indexOf(s1Arr[i]);
+      let idx2 = s2Arr.indexOf(s1Arr[i]);
+      s1Arr.splice(idx1, 1)
+      s2Arr.splice(idx2, 1)
+      i = -1;
+    }
+    if (!s1Arr.length) return result;
+  }
+  return result;
 }
+
+// console.log(getCommonCharacterCount('aabcc', 'adcaa'))  // 3
+// console.log(getCommonCharacterCount('zzzz', 'zzzzzzz')) // 4
+// console.log(getCommonCharacterCount('abca', 'xyzbac')) // 3
+// console.log(getCommonCharacterCount('', 'abc')) // 0
+// console.log(getCommonCharacterCount('a', 'b')) //0
+
