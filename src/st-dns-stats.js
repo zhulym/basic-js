494e6ca4c9df80506ad09808cb098da25bb434c1
zhulym <zhulym@mail.ru>
2021-09-12 11:38:35 +0300

    feat: task dns-stats

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,7 +22,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  let result = {};
+  let exist = [];
+  let str = domains.join('');
+  let domainsElem = domains.map(el => el.split('.').reverse()).flat();
+
+  domainsElem.forEach(domain => {
+    let regexp = new RegExp(`${domain}`, 'g');
+    let amount = str.match(regexp);
+    let keys = Object.keys(result);
+    if (!exist.includes(domain)) {
+      result[`${keys[keys.length - 1] || ''}.${domain}`] = amount.length;
+    }
+    exist.push(domain);
+  })
+  return result;
 }
+
+// console.log(getDNSStats(['epam.com', 'info.epam.com'])); //{ '.com': 2, '.com.epam': 2, '.com.epam.info': 1 }
+// console.log(getDNSStats([])); //{}
